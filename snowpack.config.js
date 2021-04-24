f1ec1b545c62592813373bf43a43be57f6099884
Leo Sandström <leo.sandstrom@realsprint.com>
2021-04-24 17:17:51 +0200

    Add base=/ to index.html

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: ["@snowpack/plugin-sass"],
+  plugins: ["@snowpack/plugin-sass", "@snowpack/plugin-typescript"],
   mount: {
     public: { url: "/", static: true },
     src: "/",
